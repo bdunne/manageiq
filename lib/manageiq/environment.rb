9ff36d079c62753ed3f656c7c884a5cd59a4e84b
Brandon Dunne <bdunne@redhat.com>
2017-11-14 13:22:37 -0500

    PAUSED: Use `git resume` to continue working.

--- lib/manageiq/environment.rb
+++ lib/manageiq/environment.rb
@@ -58,7 +58,7 @@ module ManageIQ
     def self.install_bundler(root = APP_ROOT)
       system!("echo 'gem: --no-ri --no-rdoc --no-document' > ~/.gemrc") if ENV['CI']
       system!("gem install bundler -v '#{bundler_version}' --conservative")
-      system!("bundle config path #{root.join('vendor/bundle').expand_path}", :chdir => root) if ENV["CI"]
+      # system!("bundle config path #{root.join('vendor/bundle').expand_path}", :chdir => root) if ENV["CI"]
     end
 
     def self.bundle_update(root = APP_ROOT)
